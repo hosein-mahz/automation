796bf2037e7e700cefea179d1082871c52b9559f
mohammad hossain zarei <hoeinmhz986@gmail.com>
2019-12-06 22:49:13 +0330

    .

--- patient/models.py
+++ patient/models.py
@@ -5,11 +5,11 @@ PATIENT_CHOISE = [
     (2, 'women'),
 ]
 class Patient (models.Model):
-    name = models.CharField(max_length=100)
-    family = models.CharField(max_length=100)
-    national_code = models.CharField(max_length=15)
-    birthday = models.DateTimeField(auto_now=True)
-    gender = models.IntegerField(choices=PATIENT_CHOISE)
+    name           = models.CharField(max_length=100)
+    family         = models.CharField(max_length=100)
+    national_code  = models.CharField(max_length=15)
+    birthday       = models.DateTimeField(auto_now=True)
+    gender         = models.IntegerField(choices=PATIENT_CHOISE)
     class Meta:
         ordering = ('name',)
 
@@ -23,13 +23,13 @@ CONTACT_CHOISE = [
 ]
 
 class Contact (models.Model):
-    patient_id = models.ForeignKey(Patient, null=True ,on_delete =True)
-    kay =  models.IntegerField(choices=CONTACT_CHOISE)
-    value = models.TextField(verbose_name= None )
+    patient_id  = models.ForeignKey(Patient, null=True ,on_delete=True)
+    kay         = models.IntegerField(choices=CONTACT_CHOISE)
+    value       = models.TextField(verbose_name= None )
 class Refrence (models.Model):
-    patient_id = models.ForeignKey(Patient, null=True,on_delete =True )                            
-    name = models.CharField(max_length=150)
-    phone = models.CharField(max_length=30)
+    patient_id  = models.ForeignKey(Patient, null =True,on_delete  =True )
+    name        = models.CharField(max_length=150)
+    phone       = models.CharField(max_length=30)
     # dare = models.CharField(max_length=30)
 
     def __str__(self):
@@ -43,12 +43,12 @@ RECORD_CHOISE = [
 ]
 
 class Record (models.Model):
-    patient_id = models.ForeignKey(Patient, null=True ,on_delete =True )
-    time = models.DateTimeField(auto_now=True)
-    key = models.IntegerField(max_length=160, choices=RECORD_CHOISE)
-    value = models.TextField(verbose_name= None )
-    start_date = models.DateTimeField(auto_now=True)
-    end_date = models.DateTimeField(auto_now=True)
+    patient_id  = models.ForeignKey(Patient, null =True ,on_delete=True )
+    time        = models.DateTimeField(auto_now=True)
+    key         = models.IntegerField(max_length=160, choices=RECORD_CHOISE)
+    value       = models.TextField(verbose_name= None )
+    start_date  = models.DateTimeField(auto_now=True)
+    end_date    = models.DateTimeField(auto_now=True)
 
     class meta:
         ordering = ('start_data')
